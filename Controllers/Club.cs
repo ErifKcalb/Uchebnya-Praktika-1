847087aa22a61e3493cee6783e6c6f3ff4797adc
Cyril <kurochkink@yandex.ru>
2021-10-14 00:05:50 +0300

    14.10.2021

---
+++ Controllers/Club.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Uchebnya_Praktika_1.Controllers
+{
+    
+    [ApiController]
+    [Route("Club")]
+    public class ClubController : ControllerBase
+    {
+
+
+
+
+        [HttpGet]
+        [Route("SearchManager")]
+        public string SearchManager(string str)
+        {
+            return str; //Поиск менеджера
+        }
+
+        [HttpGet]
+        [Route("BuyFootballPlayer")]
+        public string BuyFootballPlayer(string str)
+        {
+            return str; //Купить футболиста
+        }
+        }
+    }
+
+
+
+
+    
+
---
+++ Controllers/Manager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Uchebnya_Praktika_1.Controllers
+{
+    [ApiController]
+    [Route("Manager")]
+    public class ManagerController : ControllerBase
+
+    {
+        [HttpGet]
+        [Route("SearchPlaye")]
+        public string SearchPlayer(string str)
+        {
+            return str; //"ПоискФутболиста"
+        }
+
+        [HttpGet]
+        [Route("OfferContract")]
+        public string OfferContract(string str)
+        {
+            return str; //Предложить контракт
+        }
+
+        [HttpGet]
+        [Route("OfferInJoinTheClub")]
+        public string OfferInJoinTheClub(string str)
+        {
+            return str; //Предложить переход в клуб
+        }
+
+        [HttpGet]
+        [Route("SuggestPlayer")]
+        public string SuggestPlayer(string str)
+        {
+            return str; //Предложить игрока
+        }
+
+        [HttpGet]
+        [Route("SearchClub")]
+        public string SearchClub(string str)
+        {
+            return str; //Найти клуб
+        }
+    }
+}
+
+
+
+ 
+    
+
+
+
+
+
+
+
+
+
+
+
